70a6a0196dcf7f775a157e3156996804b46b258f
I-Sheng Fang <nf0126@gmail.com>
2020-11-17 11:12:15 +0800

    Update conda_gpu.sh

--- conda_gpu.sh
+++ conda_gpu.sh
@@ -2,6 +2,8 @@
 cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b
+source ~/miniconda3/bin/activate
+conda init
 
 source ~/.bashrc
 
