4d2c788099df2bf8d137ff478e8ffd3e8047811f
I-Sheng Fang <nf0126@gmail.com>
2020-11-17 10:59:19 +0800

    Update ubuntu_cpu.sh

--- ubuntu_cpu.sh
+++ ubuntu_cpu.sh
@@ -1,38 +1,36 @@
 # Basic unix tool
-sudo apt install htop git vim tmux lm-sensors at
+sudo apt install htop git vim tmux lm-sensors
+#archey 4 
+https://github.com/HorlogeSkynet/archey4/releases/download/v4.8.1/archey4_4.8.1-1_all.deb
+apt install ./archey4_4.Y.Z-R_all.deb
+
 # set vim and bash
 cp .vimrc ~/
 cp -r .vim ~/
 cp .bashrc ~/
-# install python from anaconda
+
+# install python from minicinda
 cd ~
-wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash ~/Anaconda3-5.0.1-Linux-x86_64.sh
-rm Anaconda3-5.0.1-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
 
 source ~/.bashrc
-pip install tensorflow keras pyarchey
 
-conda create -n py27 python=2 anaconda
-source activate py27
-pip install tensorflow keras
-python -m ipykernel install --user --name py27 --display-name "Python 2"
-source deactivate
+# install numpy
+conda install -y numpy matplotlib 
+# install pytorch
+conda install -y pytorch torchvision torchaudio -c pytorch
+# install tensorboard
+conda install -y tensorboard
 
 user=$USER
 user_cap=${user^^}
 
 echo "
 
-
 All Done!!!
 
 
 
 WELCOME BACK $user_cap
-
-
-
-"
-pyarchey
 
