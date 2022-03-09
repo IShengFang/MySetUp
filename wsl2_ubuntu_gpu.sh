6b115338cc905447b3a823fb497b1c188b51f89c
I-Sheng Fang <nf0126@gmail.com>
2022-03-09 12:06:51 +0800

    Create wsl2_ubuntu_gpu.sh

---
+++ wsl2_ubuntu_gpu.sh
@@ -0,0 +1,38 @@
+# Basic unix tool
+sudo apt install htop git vim tmux nvtop
+#archey 4 
+wget https://github.com/HorlogeSkynet/archey4/releases/download/v4.13.3/archey4_4.13.3-1_all.deb
+sudo apt install ./archey4_4.13.3-1_all.deb
+
+# set bash rc
+echo 'PS1="\`if [ \$? = 0 ]; then echo \[\e[32m\][NICE] ^_^ \[\e[0m\]; else echo \[\e[31m\][9487] O_O \[\e[0m\]; fi\` \u@\h:[\w]$"
+alias ptt="ssh bbsu@ptt.cc"
+alias Watch_GPU="watch -n 0.5 nvidia-smi"
+#For git (form https://stackoverflow.com/questions/5361019/viewing-full-version-tree-in-git )
+alias gl="git log --oneline --graph --decorate --all" #Show the commit histroy for tracing version
+' >> .bashrc
+
+# install python from minicinda
+cd ~
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
+source ~/miniconda3/bin/activate
+conda init
+
+echo 'archey' >> .bashrc
+# load update bashrc
+source ~/.bashrc
+
+# install pytorch w/ cuda 10.2
+conda install -y pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+# install tensorboard  matplotlib scipy
+conda install -y tensorboard matplotlib scipy
+
+user=$USER
+user_cap=${user^^}
+
+echo "
+All Done!!!
+WELCOME BACK $user_cap
+"
+archey
