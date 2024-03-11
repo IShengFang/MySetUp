06da916770fce0dbbeba740e8c6707ff960bdbbe
I-Sheng Fang <nf0126@gmail.com>
2024-03-11 16:43:57 +0800

    Create ubuntu_no_sudo_gpu.sh

---
+++ ubuntu_no_sudo_gpu.sh
@@ -0,0 +1,29 @@
+# set vim and bash
+cp .vimrc ~/
+cp -r .vim ~/
+cp .bashrc_nosudo ~/.bashrc
+
+# install python from minicinda
+cd ~
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
+source ~/miniconda3/bin/activate
+conda init
+
+source ~/.bashrc
+
+user=$USER
+user_cap=${user^^}
+
+echo "
+
+All Done!!!
+
+
+
+WELCOME BACK $user_cap
+
+
+
+"
+archey
